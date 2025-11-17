1b396e2c38352678265287305dc129b37902febb
Marcelo Raineri <mraineridev@gmail.com>
2025-11-17 15:38:57 -0300

    feat: implement Context class with AsyncLocalStorage for managing context state

---
+++ index.ts
@@ -0,0 +1,38 @@
+import { AsyncLocalStorage } from 'node:async_hooks';
+
+type objectStorageType = {
+    [key: string] : unknown
+}
+
+export class Context {
+  static asyncLocalStorageInstance: AsyncLocalStorage<unknown>;
+
+  private constructor() {
+    Context.asyncLocalStorageInstance = new AsyncLocalStorage();
+  }
+
+  static getInstance() {
+    if (!Context.asyncLocalStorageInstance) {
+      new Context();
+    }
+    return Context.asyncLocalStorageInstance;
+  }
+
+  static addValue(key: string, value: any) {
+    const contextObject = Context.getInstance().getStore() as Record<string, any>;
+    if (!contextObject)
+      throw new Error('Nenhum contexto ativo encontrado. Use Context.getInstance().run().');
+
+    contextObject[key] = value;
+    return contextObject;
+  }
+
+  static addObjectValue(object: Record<string, any>) {
+    const contextObject = Context.getInstance().getStore();
+    if (!contextObject)
+      throw new Error('Nenhum contexto ativo encontrado. Use Context.getInstance().run().');
+
+    const merged = Object.assign(contextObject, object);
+    return merged;
+  }
+} 
