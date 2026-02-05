ee9c773d4f477fe2a1bcce21966471a619980668
Marcelo Raineri <mraineridev@gmail.com>
2026-02-05 09:58:31 -0300

    feat: export AsyncContextNestMiddleware from index.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { Context } from "./core/context";
 export { AsyncContextExpresssMiddleware } from "./core/integrations/express";
+export { AsyncContextNestMiddleware } from "./core/integrations/nest";
