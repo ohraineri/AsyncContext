1aa55e1863cd5e88323d8e6f0044d0005c45654c
Marcelo Raineri <mraineridev@gmail.com>
2026-02-05 10:17:51 -0300

    feat: export AsyncContextAdonisMiddleware from core/integrations/adonis

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { Context } from "./core/context";
 export { AsyncContextExpresssMiddleware } from "./core/integrations/express";
 export { AsyncContextNestMiddleware } from "./core/integrations/nest";
+export { AsyncContextAdonisMiddleware } from "./core/integrations/adonis";
