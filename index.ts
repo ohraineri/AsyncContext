41a7121b80c0df542ca1e81790a5f6f1503bc37b
Marcelo Raineri <mraineridev@gmail.com>
2026-02-06 07:46:44 -0300

    feat: add logging exports to index.ts for enhanced logging capabilities

--- index.ts
+++ index.ts
@@ -8,3 +8,15 @@ export {
 } from "./core/integrations/express";
 export { AsyncContextNestMiddleware } from "./core/integrations/nest";
 export { AsyncContextAdonisMiddleware } from "./core/integrations/adonis";
+export {
+  Logger,
+  createLogger,
+  createConsoleTransport,
+  type LogLevel,
+  type LogData,
+  type LogEntry,
+  type LoggerOptions,
+  type Transport,
+  type ConsoleTransportOptions,
+  type SerializedError,
+} from "./core/logging/logger";
