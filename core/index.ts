42f8abca0a0909b7d8fb162efa4e84317469553f
Marcelo Raineri <mraineridev@gmail.com>
2026-02-06 08:13:01 -0300

    feat: export Sentry integration functions and types from core

--- core/index.ts
+++ core/index.ts
@@ -2,3 +2,18 @@ export { Context } from './context';
 export { AsyncContextExpresssMiddleware } from './integrations/express';
 export { AsyncContextNestMiddleware } from './integrations/nest';
 export { AsyncContextAdonisMiddleware } from './integrations/adonis';
+export {
+  bindAsyncContextToSentryScope,
+  captureExceptionWithContext,
+  initSentryWithAsyncContext,
+  sentryAsyncContextExpressMiddleware,
+  sentryErrorHandler,
+} from './integrations/sentry';
+export type {
+  InitSentryOptions,
+  SentryAsyncContextOptions,
+  SentryKeyMapping,
+  SentryLike,
+  SentryScopeLike,
+  SentryUserMapping,
+} from './integrations/sentry';
--- index.ts
+++ index.ts
@@ -2,3 +2,18 @@ export { Context } from "./core/context";
 export { AsyncContextExpresssMiddleware } from "./core/integrations/express";
 export { AsyncContextNestMiddleware } from "./core/integrations/nest";
 export { AsyncContextAdonisMiddleware } from "./core/integrations/adonis";
+export {
+  bindAsyncContextToSentryScope,
+  captureExceptionWithContext,
+  initSentryWithAsyncContext,
+  sentryAsyncContextExpressMiddleware,
+  sentryErrorHandler,
+} from "./core/integrations/sentry";
+export type {
+  InitSentryOptions,
+  SentryAsyncContextOptions,
+  SentryKeyMapping,
+  SentryLike,
+  SentryScopeLike,
+  SentryUserMapping,
+} from "./core/integrations/sentry";
