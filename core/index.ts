7704b9f4c249b9dae6b14a05eee9826b7bec67ff
Marcelo Raineri <mraineridev@gmail.com>
2026-02-06 07:46:40 -0300

    feat: update index.ts exports to include additional types and middleware for enhanced context management

--- core/index.ts
+++ core/index.ts
@@ -1,4 +1,22 @@
-export { Context } from './context';
-export { AsyncContextExpresssMiddleware } from './integrations/express';
-export { AsyncContextNestMiddleware } from './integrations/nest';
-export { AsyncContextAdonisMiddleware } from './integrations/adonis';
+export { Context, type ContextStore } from "./context";
+export {
+  AsyncContextExpresssMiddleware,
+  AsyncContextExpressMiddleware,
+  createAsyncContextExpressMiddleware,
+  type AsyncContextExpressOptions,
+  type AsyncContextExpressSeed,
+} from "./integrations/express";
+export { AsyncContextNestMiddleware } from "./integrations/nest";
+export { AsyncContextAdonisMiddleware } from "./integrations/adonis";
+export {
+  Logger,
+  createLogger,
+  createConsoleTransport,
+  type LogLevel,
+  type LogData,
+  type LogEntry,
+  type LoggerOptions,
+  type Transport,
+  type ConsoleTransportOptions,
+  type SerializedError,
+} from "./logging/logger";
